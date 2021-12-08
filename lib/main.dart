83ce219c46b9dfcba3257e949cff95930aa4e1a2
carolrosatto <carolrosatto@gmail.com>
2021-12-08 14:21:55 -0300

    re-commiting project in new repo

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+import 'screens/dashboard.dart';
+
+void main() {
+  runApp(const ByteBankApp());
+}
+
+class ByteBankApp extends StatelessWidget {
+  const ByteBankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.green.shade900,
+            secondary: Colors.blueAccent.shade700),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.blueAccent.shade700),
+          ),
+        ),
+      ),
+      home: Dashboard(),
+    );
+  }
+}
